60e711603b33a4127341323ddeba0ea94b382afa
Puk0123 <sheshkovas12@yandex.ru>
2023-10-28 10:31:46 +0300

    update utils 5 (add docstrings)

--- utils.py
+++ utils.py
@@ -1,3 +1,14 @@
+"""
+Требоания:
+Последние 5 выполненных (EXECUTED) операций выведены на экран.
+Операции разделены пустой строкой.
+Дата перевода представлена в формате ДД.ММ.ГГГГ (пример: 14.10.2018).
+Сверху списка находятся самые последние операции (по дате).
+Номер карты замаскирован и не отображается целиком в формате  XXXX XX** **** XXXX (видны первые 6 цифр и последние 4, разбито по блокам по 4 цифры, разделенных пробелом).
+Номер счета замаскирован и не отображается целиком в формате  **XXXX
+(видны только последние 4 цифры номера счета).
+"""
+
 import json
 from datetime import datetime
 import os
@@ -5,13 +16,18 @@ import os
 print(os.getcwd())
 
 def get_data(filename):
+    """
+    Получает данные из operations.json
+    """
     with open(filename, 'r') as file:
         data: object = json.load(file)
         return data
 
 
-
 def get_operations_executed(data):
+    """
+    Проверяет статус перевода
+    """
     operations_executed= []
     for operation in data:
         if 'state' in operation and operation['state'] == 'EXECUTED':
@@ -24,12 +40,18 @@ def get_operations_executed(data):
 
 
 def get_last_num_operations(operations_with_from, num_of_operations):
+    """
+    Проверяет последние операции
+    """
     operations_sort = sorted(operations_with_from, key=lambda operation: operation["date"], reverse=True)
     last_five_operations = operations_sort[0:num_of_operations]
     return last_five_operations
 
 
 def get_operations_formatted(last_five_operations):
+    """
+    Форматирует последние 5 операций в соответствие с требованиями
+    """
     operations_formatted_list = []
     for operation in last_five_operations:
         date = datetime.strptime(operation['date'], '%Y-%m-%dT%H:%M:%S.%f').strftime('%d.%m.%Y')
