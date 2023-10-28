baf963e9903878e724a5fe58b8a82c248aff4c8b
Puk0123 <sheshkovas12@yandex.ru>
2023-10-28 10:20:45 +0300

    update utils 1

--- utils.py
+++ utils.py
@@ -8,3 +8,16 @@ def get_data(filename):
     with open(filename, 'r') as file:
         data: object = json.load(file)
         return data
+
+
+
+def get_operations_executed(data):
+    operations_executed= []
+    for operation in data:
+        if 'state' in operation and operation['state'] == 'EXECUTED':
+            operations_executed.append(operation)
+        operation_with_from = []
+        for operation in operations_executed:
+            if 'from' in operation:
+                operation_with_from.append(operation)
+    return operation_with_from
